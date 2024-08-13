17f82202c08b4edb5d85eb8e61ddf82df361f493
Viturna <viturnapro@gmail.com>
2024-08-13 21:11:19 +0200

    puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,16 @@
 # config/puma.rb
+
 workers 2
 threads 1, 6
 
 preload_app!
 
-port 3000
-environment ENV['RAILS_ENV'] || 'development'
+port ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
 
 on_worker_boot do
-  ActiveRecord::Base.establish_connection
+  # Worker specific setup for Rails 4.1+
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
 end

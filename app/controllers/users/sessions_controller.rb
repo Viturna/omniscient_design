8c6851e18fde1825e2f4b066f05b414ee1f3d9fe
Viturna <viturnapro@gmail.com>
2024-08-16 13:59:37 +0200

    devise

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,16 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   before_action :configure_permitted_parameters, if: :devise_controller?
-  def create
-    self.resource = warden.authenticate!(auth_options)
 
-    if !resource.confirmed?
-      resend_link = view_context.link_to('Renvoyer l\'email de confirmation', new_user_confirmation_path)
-      set_flash_message! :alert, :unconfirmed, resend_link: resend_link
-      respond_with_navigational(resource) { render :new }
-    else
-      super
-    end
-  end
   protected
 
   def configure_permitted_parameters

25fc44eae30f7d9c1441ddf1ad331f090f7efdb9
Viturna <viturnapro@gmail.com>
2024-12-08 19:23:05 +0100

    Responsive refrence / Loader / Font / Button

--- app/javascript/controllers/referenceScroll.js
+++ app/javascript/controllers/referenceScroll.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(window).scroll(function() {
+  $(window).on('scroll', function() {
     var currentScroll = $(window).scrollTop();
 
     if (currentScroll > 30) {
@@ -7,13 +7,13 @@ $(document).ready(function() {
       $('.header-top').addClass('show-header-top');
       $('.hidden').addClass('hidden');
       $('.info-header-show').addClass('hidden');
-      $('.content-show').css('margin-top', '50vh');
+      $('.content-show').css('margin-top', '50vh'); // Ajuste la marge en fonction du scroll
     } else {
       $('.header-show').removeClass('small-height');
       $('.header-top').removeClass('show-header-top');
       $('.hidden').removeClass('hidden');
       $('.info-header-show').removeClass('hidden');
-      $('.content-show').css('margin-top', '20px');
+      $('.content-show').css('margin-top', '20px'); // Reviens à la valeur par défaut
     }
   });
 });

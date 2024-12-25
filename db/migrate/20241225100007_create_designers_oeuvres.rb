e019b2b4fab0deae643a3bacb348cf41acee14f5
Viturna <viturnapro@gmail.com>
2024-12-25 15:06:16 +0100

    DB Migration

--- db/migrate/20241225100007_create_designers_oeuvres.rb
+++ db/migrate/20241225100007_create_designers_oeuvres.rb
@@ -1,13 +1,13 @@
 class CreateDesignersOeuvres < ActiveRecord::Migration[7.1]
   def change
+    unless table_exists?(:designers_oeuvres)
     create_table :designers_oeuvres do |t|
       t.references :designer, null: false, foreign_key: true
       t.references :oeuvre, null: false, foreign_key: true
 
       t.timestamps
+      end
     end
-
-    # Empêcher les doublons dans la relation
     add_index :designers_oeuvres, [:designer_id, :oeuvre_id], unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_23_204513) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_25_100007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 

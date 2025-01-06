f345b5e5ff1c36cbc963da21eaa29d33526f0a90
Viturna <viturnapro@gmail.com>
2025-01-06 21:28:14 +0100

    feedbacks corrections

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,7 +1,6 @@
 class Feedback < ApplicationRecord
   belongs_to :user
 
-  validates :question_1, :question_2, :question_3, :question_4, :question_5, :question_6, :question_7, presence: true
-  validates :question_8, :question_9, :question_10, :question_11, :question_12, presence: true
+  validates :question_1, :question_2, :question_3, :question_5, :question_6, :question_7, presence: true
   validates :user, presence: true
 end

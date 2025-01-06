0ddbbd720a22cbe7faa80d51a1e73d25a1c922c4
Viturna <viturnapro@gmail.com>
2025-01-06 23:56:27 +0100

    info références/designers scroll

--- app/javascript/controllers/referenceScroll.js
+++ app/javascript/controllers/referenceScroll.js
@@ -6,13 +6,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
     const header = document.querySelector('.header-show');
     const text = document.querySelector('.text-show');
+    const infos = document.querySelector('.header-top');
+    const firstContent = this.document.querySelector('.info-card-first-content')
 
     if (currentScroll > lastScrollTop && currentScroll > scrollThreshold) {
       // Scroll vers le bas et dépasse le seuil
+      firstContent.classList.add('hidden')
+      infos.classList.add('show-header-top')
       header.classList.add('small-height');
       text.classList.add('small-height');
     } else if (currentScroll <= scrollThreshold) {
       // Scroll vers le haut et en dessous du seuil
+      firstContent.classList.remove('hidden')
+      infos.classList.remove('show-header-top')
       header.classList.remove('small-height');
       text.classList.remove('small-height');
     }

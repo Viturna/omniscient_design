941409395256032217bf04a00707f65869f9cf6b
Viturna <viturnapro@gmail.com>
2025-01-07 00:15:19 +0100

    list create img

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,10 @@ Rails.application.routes.draw do
       delete 'remove_user'
       post 'toggle_privacy'
     end
+    collection do
+      get :lazy_load_designers
+      get :lazy_load_oeuvres
+    end
   end
 
   get '/shared/:share_token', to: 'lists#shared', as: :shared_list

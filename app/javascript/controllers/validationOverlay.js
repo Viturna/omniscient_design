620a03130c89757b6d6254b4fc8bbda17eddf262
Viturna <viturnapro@gmail.com>
2025-01-06 23:38:51 +0100

    Image bel air corrigé / Search category image correction / Button delai transition 1s -> 0.3s

--- app/javascript/controllers/validationOverlay.js
+++ app/javascript/controllers/validationOverlay.js
@@ -1,20 +1,23 @@
-const validation = document.getElementById('validation');
-const overlay = document.getElementById('overlay');
+document.addEventListener('DOMContentLoaded', () => {
+  const validation = document.getElementById('validation');
+  const overlay = document.getElementById('overlay');
+  const overlaybottom = document.getElementById('overlay-bottom');
 
-function hideValidation() {
-  validation.style.display = 'none';
-  overlay.style.display = 'none';
-  window.removeEventListener('scroll', hideValidation);
-}
+  function hideValidation() {
+    if (validation && overlay) {
+      validation.style.display = 'none';
+      overlay.style.display = 'none';
+      window.removeEventListener('scroll', hideValidation);
+    }
+  }
 
-window.addEventListener('scroll', hideValidation);
+  function seenValidation() {
+    if (overlaybottom) {
+      overlaybottom.style.display = 'block';
+      window.removeEventListener('scroll', seenValidation);
+    }
+  }
 
-
-const overlaybottom = document.getElementById('overlay-bottom');
-
-function seenValidation() {
-  overlaybottom.style.display = 'block';
-  window.removeEventListener('scroll', seenValidation);
-}
-
-window.addEventListener('scroll', seenValidation);
+  window.addEventListener('scroll', hideValidation);
+  window.addEventListener('scroll', seenValidation);
+});

360f4a0d3fdefdf608b6535774b8f2e464e3f532
Viturna <148191882+Viturna@users.noreply.github.com>
2025-02-24 12:33:04 +0100

    Correction DB

--- db/migrate/20250223182007_create_notions_oeuvres.rb
+++ db/migrate/20250223182007_create_notions_oeuvres.rb
@@ -1,11 +1,10 @@
 class CreateNotionsOeuvres < ActiveRecord::Migration[7.1]
   def change
-    create_table :notions_oeuvres, id: false do |t|
+    create_table :notions_oeuvres do |t|
       t.references :oeuvre, null: false, foreign_key: true
       t.references :notion, null: false, foreign_key: true
-    end
 
-    add_index :notions_oeuvres, [:oeuvre_id, :notion_id], unique: true
-    add_index :notions_oeuvres, [:notion_id, :oeuvre_id], unique: true
+      t.timestamps
+    end
   end
 end

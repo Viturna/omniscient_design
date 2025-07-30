de11c632997a4107e2537c7d6c259f10fe7bc39d
Viturna <148191882+Viturna@users.noreply.github.com>
2025-07-30 22:27:31 +0200

    Update users_controller.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,49 +1,36 @@
-module Api
-  module V1
-    class UsersController < ApplicationController
-      before_action :authenticate_user!
-      before_action :set_user, only: [:show, :update, :destroy]
+# app/controllers/api/v1/sessions_controller.rb
+class Api::V1::SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token # important pour API
+  before_action :set_default_format
 
-      def index
-        @users = User.all
-        render json: @users
-      end
-
-      def show
-        render json: @user
-      end
-
-      def create
-        @user = User.new(user_params)
-        if @user.save
-          render json: @user, status: :created
-        else
-          render json: @user.errors, status: :unprocessable_entity
-        end
-      end
-
-      def update
-        if @user.update(user_params)
-          render json: @user
-        else
-          render json: @user.errors, status: :unprocessable_entity
-        end
-      end
-
-      def destroy
-        @user.destroy
-        head :no_content
-      end
-
-      private
-
-      def set_user
-        @user = User.find(params[:id])
-      end
-
-      def user_params
-        params.require(:user).permit(:email, :firstname, :lastname, :role, :certified, :banned)
-      end
+  def create
+    user = User.find_for_authentication(email: params[:email])
+    if user&.valid_password?(params[:password])
+      render json: {
+        user: user.slice(:id, :email, :role),
+        token: user.id.to_s # ou JWT si tu veux
+      }, status: :ok
+    else
+      render json: { error: 'Email ou mot de passe invalide' }, status: :unauthorized
     end
+  end
+
+  def show
+    user = User.find_by(id: request.headers['Authorization'])
+    if user
+      render json: { user: user.slice(:id, :email, :role) }, status: :ok
+    else
+      render json: { error: 'Non connecté' }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    head :no_content
+  end
+
+  private
+
+  def set_default_format
+    request.format = :json
   end
 end

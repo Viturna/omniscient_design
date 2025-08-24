58a678a7a19b197eb38b5fea983a69fb2216d580
Viturna <148191882+Viturna@users.noreply.github.com>
2025-08-25 00:00:38 +0200

    Update manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,4 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-
-// facultatif si tu veux exposer ton favicon et manifest
+//= link application.js
 //= link favicon/site.webmanifest

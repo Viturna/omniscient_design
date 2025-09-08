adb4acf084fb2fa2fae101d3e31a786524c29a8e
Viturna <148191882+Viturna@users.noreply.github.com>
2025-09-08 14:56:44 +0200

    Update manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,3 @@
 //= link_tree ../images
-//= link application.js
+//= link_directory ../stylesheets .css
 //= link favicon/site.webmanifest
-//= link_tree ../../javascript/controllers
-//= link_tree ../builds

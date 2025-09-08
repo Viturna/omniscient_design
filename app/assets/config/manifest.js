fb7cb2e241ebcb0e746591b537a2638ee9236718
Viturna <148191882+Viturna@users.noreply.github.com>
2025-09-08 21:23:49 +0200

    Update manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,11 @@
 //= link_directory ../stylesheets .css
 //= link favicon/site.webmanifest
 //= link application.js
+
+//= link @hotwired--turbo-rails.js
+
+//= link_tree ../../javascript/controllers
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link rails-ujs.js
+//= link jquery.min.js

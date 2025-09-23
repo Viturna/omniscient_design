661f38f4a1ed9c0054644014950db7a285dca025
Viturna <148191882+Viturna@users.noreply.github.com>
2025-09-23 23:01:59 +0200

    js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -13,3 +13,14 @@ import "select2"
 
 import "gsap"
 import "gsap/ScrollTrigger"
+
+
+document.addEventListener("turbo:load", () => {
+    console.log("✅ Turbo load : scripts relancés")
+
+    // Ré-init select2
+    if ($(".select2").length) {
+        $(".select2").select2()
+    }
+
+})

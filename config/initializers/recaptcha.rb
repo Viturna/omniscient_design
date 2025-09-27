cee6de59fda6fca8df4a71a7dfc3b75431dd1bda
Viturna <148191882+Viturna@users.noreply.github.com>
2025-09-27 19:01:34 +0200

    captcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key = Rails.application.credentials.dig(:recaptcha, :site_key)
-  config.secret_key = Rails.application.credentials.dig(:recaptcha, :secret_key)
+  config.site_key  = ENV["RECAPTCHA_SITE_KEY"] || Rails.application.credentials.dig(:recaptcha, :site_key)
+  config.secret_key = ENV["RECAPTCHA_SECRET_KEY"] || Rails.application.credentials.dig(:recaptcha, :secret_key)
 end

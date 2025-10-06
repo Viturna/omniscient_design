5f366a61d1753e37c7b01c784e593ae2b132f0aa
Viturna <148191882+Viturna@users.noreply.github.com>
2025-10-06 17:36:43 +0200

    mail

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,11 +1,6 @@
-# test/mailers/previews/user_mailer_preview.rb
 class UserMailerPreview < ActionMailer::Preview
-  # Prévisualisation de l’e-mail de réinitialisation du mot de passe
-  def reset_password_instructions
+  def email_changed
     user = User.first || User.new(email: "exemple@demo.com")
-    token = "faux_token_123"
-
-    # S'assurer d'utiliser le mailer de Devise
-    Devise.mailer.reset_password_instructions(user, token)
+    Devise.mailer.email_changed(user)
   end
 end

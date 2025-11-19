1438a9ebd580cd37901376fad39b6cb6a5c93051
Viturna <148191882+Viturna@users.noreply.github.com>
2025-11-19 18:00:46 +0100

    perf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,12 @@ Rails.application.configure do
   # config.require_master_key = true
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
-  # config.public_file_server.enabled = false
+  config.public_file_server.enabled = true
+
+  config.public_file_server.headers = {
+  "Cache-Control" => "public, max-age=31536000",
+  "Expires" => 1.year.from_now.to_fs(:rfc822)
+}
 
   # Compress CSS using a preprocessor.
   config.assets.css_compressor = :sass

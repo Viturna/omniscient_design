fa2a70134b5864b9056b62f9bec210c2d796b6a9
Viturna <148191882+Viturna@users.noreply.github.com>
2025-11-25 11:52:08 +0100

    Loader error

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,22 +1,18 @@
-# Application
-pin "application", preload: true
+pin "application"
+
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-# IMPORTANT : On épingle le shim explicitement pour être sûr qu'il est disponible
 pin "es-module-shims", to: "https://ga.jspm.io/npm:es-module-shims@1.10.0/dist/es-module-shims.js"
 
-# Hotwire
 pin "@hotwired/turbo", to: "@hotwired--turbo.js"
 pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js"
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
-# jQuery (Via JSPM)
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
 pin "jquery_provider", to: "jquery_provider.js"
 pin "select2", to: "https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"
 
-# Autres
 pin "gsap", to: "https://cdn.jsdelivr.net/npm/gsap@3.12.5/index.js"
 pin "@rails/actioncable/src", to: "@rails--actioncable--src.js"
 pin "sortablejs", to: "https://ga.jspm.io/npm:sortablejs@1.15.2/modular/sortable.esm.js"
--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,16 +1,18 @@
 Rails.application.configure do
   config.content_security_policy do |policy|
     policy.default_src :self, :https, :http, :unsafe_inline
-    policy.font_src    :self, :https, :data
+    
+    # FONTS : On autorise data: et le domaine actuel
+    policy.font_src    :self, :https, :http, :data
+    
     policy.img_src     :self, :https, :http, :data, :blob, "http://localhost:9000"
     policy.object_src  :none
     
-    # On autorise tout pour être sûr que le Shim et les maps chargent
+    # SCRIPTS : On garde :blob et unsafe_eval/inline
     policy.script_src  :self, :https, :http, :unsafe_inline, :unsafe_eval, :blob, "https://cdn.jsdelivr.net", "https://ga.jspm.io"
     
     policy.style_src   :self, :https, :http, :unsafe_inline
   end
 
-  # Désactivation des Nonces pour éviter les blocages "Mappages non autorisés"
   config.content_security_policy_nonce_generator = nil
 end

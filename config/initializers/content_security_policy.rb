6829d24686fb60eb8d8a3babb38625f8e9a1eaa4
Viturna <148191882+Viturna@users.noreply.github.com>
2025-11-25 09:46:20 +0100

    ERROR LOADING

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,19 +4,19 @@ Rails.application.configure do
     policy.default_src :self, :https, :http, :unsafe_inline
     policy.font_src    :self, :https, :data
     
-    # IMAGES : On autorise explicitement localhost:9000 et le schéma http:
+    # IMAGES : On garde ta config
     policy.img_src     :self, :https, :http, :data, :blob, "http://localhost:9000"
     
     policy.object_src  :none
     
-    # SCRIPTS : On autorise tout le monde pour débloquer la situation
-    policy.script_src  :self, :https, :http, :unsafe_inline, :unsafe_eval, "https://cdn.jsdelivr.net", "https://ga.jspm.io"
+    # --- LA CORRECTION EST ICI ---
+    # J'ai ajouté :blob à la liste ci-dessous
+    policy.script_src  :self, :https, :http, :unsafe_inline, :unsafe_eval, :blob, "https://cdn.jsdelivr.net", "https://ga.jspm.io"
     
-    # STYLES : On autorise le style inline
+    # STYLES
     policy.style_src   :self, :https, :http, :unsafe_inline
   end
 
-  # --- LA SOLUTION EST ICI ---
-  # Ne commente pas cette ligne. Mets-la à NIL pour forcer la désactivation.
+  # On s'assure que les Nonces sont désactivés
   config.content_security_policy_nonce_generator = nil
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,6 @@
 Rails.application.config.session_store :cookie_store, 
+  expire_after: 1.year,
   key: '_omniscient_design_session', 
   secure: Rails.env.production?,
   same_site: (Rails.env.production? ? :none : :lax)
+  

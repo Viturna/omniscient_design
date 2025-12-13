d3b33b8f10fb417b66b47088c1a4aec9ad82c0f3
Viturna <148191882+Viturna@users.noreply.github.com>
2025-12-13 22:06:02 +0100

    Update scroll_vertical_controller.js

--- app/javascript/controllers/scroll_vertical_controller.js
+++ app/javascript/controllers/scroll_vertical_controller.js
@@ -2,29 +2,72 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
     static values = {
-        cooldown: { type: Number, default: 800 }
+        cooldown: { type: Number, default: 800 },
+        threshold: { type: Number, default: 50 }
     }
 
     connect() {
         this.isScrolling = false
-        this.handleWheel = this.handleWheel.bind(this)
 
+        this.handleWheel = this.handleWheel.bind(this)
         this.element.addEventListener("wheel", this.handleWheel, { passive: false })
+
+        this.handleTouchStart = this.handleTouchStart.bind(this)
+        this.handleTouchMove = this.handleTouchMove.bind(this)
+        this.handleTouchEnd = this.handleTouchEnd.bind(this)
+
+        this.element.addEventListener("touchstart", this.handleTouchStart, { passive: false })
+        this.element.addEventListener("touchmove", this.handleTouchMove, { passive: false })
+        this.element.addEventListener("touchend", this.handleTouchEnd)
     }
 
     disconnect() {
         this.element.removeEventListener("wheel", this.handleWheel)
+        this.element.removeEventListener("touchstart", this.handleTouchStart)
+        this.element.removeEventListener("touchmove", this.handleTouchMove)
+        this.element.removeEventListener("touchend", this.handleTouchEnd)
     }
 
     handleWheel(event) {
         event.preventDefault()
-
         if (this.isScrolling) return
+
         const deltaY = event.deltaY
         if (Math.abs(deltaY) < 30) return
+
+        this.triggerScroll(deltaY > 0 ? 1 : -1)
+    }
+
+
+    handleTouchStart(event) {
+        this.touchStartY = event.touches[0].clientY
+        this.touchStartX = event.touches[0].clientX
+    }
+
+    handleTouchMove(event) {
+        if (event.cancelable) event.preventDefault()
+    }
+
+    handleTouchEnd(event) {
+        if (this.isScrolling) return
+
+        const touchEndY = event.changedTouches[0].clientY
+        const touchEndX = event.changedTouches[0].clientX
+
+        const deltaY = this.touchStartY - touchEndY
+        const deltaX = this.touchStartX - touchEndX
+
+        if (Math.abs(deltaY) > Math.abs(deltaX)) {
+            if (Math.abs(deltaY) > this.thresholdValue) {
+                this.triggerScroll(deltaY > 0 ? 1 : -1)
+            }
+        }
+    }
+
+    triggerScroll(direction) {
         this.isScrolling = true
 
-        if (deltaY > 0) {
+        if (direction > 0) {
             this.scrollToNext()
         } else {
             this.scrollToPrev()

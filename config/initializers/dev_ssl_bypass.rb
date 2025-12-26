412e70c68a182c068a0a7889969dc646e0cba0c0
Viturna <148191882+Viturna@users.noreply.github.com>
2025-12-26 18:41:49 +0100

    save

--- config/initializers/dev_ssl_bypass.rb
+++ config/initializers/dev_ssl_bypass.rb
@@ -1,5 +1,7 @@
 if Rails.env.development?
   require 'openssl'
 
-  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+  OpenSSL::SSL.send(:remove_const, :VERIFY_PEER) if OpenSSL::SSL.const_defined?(:VERIFY_PEER)
+  
+  OpenSSL::SSL.const_set(:VERIFY_PEER, OpenSSL::SSL::VERIFY_NONE)
 end

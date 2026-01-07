42311e4383cb6822c524a5765b5777ccb16660aa
Viturna <148191882+Viturna@users.noreply.github.com>
2026-01-07 12:21:18 +0100

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,5 @@
-# config/initializers/session_store.rb
 Rails.application.config.session_store :cookie_store, 
   key: '_omniscient_design_session',
   expire_after: 1.year
+  secure: true,
+  same_site: :none

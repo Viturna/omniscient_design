719a8e68a832cf5479bc65ebe07bd9d019178313
Viturna <148191882+Viturna@users.noreply.github.com>
2026-03-07 17:47:23 +0100

    erreur 500

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,9 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link favicon/site.webmanifest
+//= link application.js
+//= link @hotwired--turbo-rails.js
+//= link_tree ../../javascript/controllers
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link rails-ujs.js

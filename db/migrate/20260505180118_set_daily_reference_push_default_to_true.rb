6c977b4d19b891272efbfa6fbd1edd1da8652d08
Viturna <148191882+Viturna@users.noreply.github.com>
2026-05-05 20:02:22 +0200

    ajout notif auto ref du jour

---
+++ db/migrate/20260505180118_set_daily_reference_push_default_to_true.rb
@@ -0,0 +1,15 @@
+class SetDailyReferencePushDefaultToTrue < ActiveRecord::Migration[8.1]
+  def up
+    # 1. Change le défaut pour les futurs utilisateurs
+    change_column_default :users, :daily_reference_push, from: false, to: true
+
+    # 2. Active la notification pour tous les utilisateurs actuels
+    User.update_all(daily_reference_push: true)
+  end
+
+  def down
+    change_column_default :users, :daily_reference_push, from: true, to: false
+    # On ne revient pas en arrière pour les données existantes pour éviter d'écraser 
+    # les choix volontaires des utilisateurs s'ils ont déjà rechangé.
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_19_224300) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_05_180118) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "unaccent"
@@ -539,7 +539,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_19_224300) do
     t.bigint "country_id"
     t.datetime "created_at", null: false
     t.boolean "daily_reference_email", default: false
-    t.boolean "daily_reference_push", default: false
+    t.boolean "daily_reference_push", default: true
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "etablissement"

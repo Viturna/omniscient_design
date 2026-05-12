3d869320e9adc5a6b30d3ea79d2da404afb87d85
Viturna <148191882+Viturna@users.noreply.github.com>
2026-05-12 23:23:08 +0200

    Update quiz_submission.rb

--- app/models/quiz_submission.rb
+++ app/models/quiz_submission.rb
@@ -2,6 +2,7 @@ class QuizSubmission < ApplicationRecord
   belongs_to :user
   belongs_to :quiz
   
+  attribute :status, :string
   enum :status, { in_progress: 'in_progress', completed: 'completed' }
 
   validates :score, numericality: { greater_than_or_equal_to: 0 }

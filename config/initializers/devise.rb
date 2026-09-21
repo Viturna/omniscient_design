589073afc0857727d66ff6635f14915f66e06b2f
Viturna <148191882+Viturna@users.noreply.github.com>
2026-09-21 12:24:08 +0200

    devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,6 +272,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+    prompt: 'select_account',
     provider_ignores_state: true
   }
   config.omniauth :apple,

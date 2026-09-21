e800d78278170bf48fb38b66df73da07be68d5f0
Viturna <148191882+Viturna@users.noreply.github.com>
2026-09-21 12:15:02 +0200

    oauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -39,15 +39,22 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if user_signed_in? && @user && @user != current_user
       flash[:alert] = "Ce compte #{kind} est déjà lié à un autre utilisateur."
-      render html: "<!DOCTYPE html><html><head><meta charset='utf-8'><script>window.location.href='omniscient://auth_failure';</script></head><body></body></html>".html_safe
+      redirect_to(native_app_request? ? "omniscient://auth_failure" : edit_user_registration_path, allow_other_host: true)
     elsif @user&.persisted?
       unless @user.confirmed?
         @user.skip_confirmation!
         @user.save!
       end
 
-      token = Rails.application.message_verifier(:mobile_auth).generate(@user.id, purpose: :mobile_login, expires_in: 5.minutes)
-      redirect_to "omniscient://auth_success?token=#{token}", allow_other_host: true
+      sign_in(:user, @user)
+
+      if native_app_request?
+        token = Rails.application.message_verifier(:mobile_auth).generate(@user.id, purpose: :mobile_login, expires_in: 5.minutes)
+        redirect_to "omniscient://auth_success?token=#{token}", allow_other_host: true
+      else
+        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: kind
+        redirect_to after_sign_in_path_for(@user)
+      end
     else
       session['devise.omniauth_data'] = auth.except('extra')
 
@@ -57,15 +64,20 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
       user = User.from_omniauth(auth) if User.respond_to?(:from_omniauth)
       if user&.persisted?
-        token = Rails.application.message_verifier(:mobile_auth).generate(user.id, purpose: :mobile_login, expires_in: 5.minutes)
-        redirect_to "omniscient://auth_success?token=#{token}", allow_other_host: true
+        sign_in(:user, user)
+        if native_app_request?
+          token = Rails.application.message_verifier(:mobile_auth).generate(user.id, purpose: :mobile_login, expires_in: 5.minutes)
+          redirect_to "omniscient://auth_success?token=#{token}", allow_other_host: true
+        else
+          redirect_to after_sign_in_path_for(user)
+        end
       else
-        redirect_to new_user_registration_url
+        redirect_to(native_app_request? ? "omniscient://auth_failure" : new_user_registration_url, allow_other_host: true)
       end
     end
   end
 
-  def native_request?
-    native_app? || request.user_agent.to_s.include?('Turbo Native') || request.env['omniauth.params']&.fetch('native_app', nil) == 'true' || params[:native_app] == 'true'
+  def native_app_request?
+    native_app? || request.user_agent.to_s.include?('Turbo Native') || session[:is_native_app] == true
   end
 end
